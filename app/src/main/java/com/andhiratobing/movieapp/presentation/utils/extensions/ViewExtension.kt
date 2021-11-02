71d0461a4a05fae95ab475ea82e6dc791d92e63c
ratobing <ratobing6@gmail.com>
2021-11-02 11:25:37 +0700

    add extension

---
+++ app/src/main/java/com/andhiratobing/movieapp/presentation/utils/extensions/ViewExtension.kt
@@ -0,0 +1,23 @@
+package com.andhiratobing.movieapp.presentation.utils.extensions
+
+import android.view.View
+
+/**
+ * Show the view  (visibility = View.VISIBLE)
+ */
+fun View.show(): View {
+    if (visibility != View.VISIBLE) {
+        visibility = View.VISIBLE
+    }
+    return this
+}
+
+/**
+ * Remove the view (visibility = View.GONE)
+ */
+fun View.hide(): View {
+    if (visibility != View.GONE) {
+        visibility = View.GONE
+    }
+    return this
+}
