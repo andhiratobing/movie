fda9867386292c35f21255004111a0ae372de9b4
ratobing <ratobing6@gmail.com>
2021-11-02 11:38:32 +0700

    add movie domain in layer domain

---
+++ app/src/main/java/com/andhiratobing/movieapp/domain/models/MovieDomain.kt
@@ -0,0 +1,12 @@
+package com.andhiratobing.movieapp.domain.models
+
+data class MovieDomain(
+    val backdropPath: String?,
+    val genreIds: List<Int>?,
+    val id: Int?,
+    val originalTitle: String?,
+    val overview: String?,
+    val posterPath: String?,
+    val releaseDate: String?,
+    val voteAverage: Double?
+)
