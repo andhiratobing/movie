03497e5ef6420e013671adc48b2a5a7dc356a69a
ratobing <ratobing6@gmail.com>
2021-11-06 09:34:00 +0700

    change import

--- app/src/main/java/com/andhiratobing/movieapp/presentation/fragments/home/adapters/MovieViewHolder.kt
+++ app/src/main/java/com/andhiratobing/movieapp/presentation/fragments/home/adapters/MovieViewHolder.kt
@@ -1,9 +1,9 @@
 package com.andhiratobing.movieapp.presentation.fragments.home.adapters
 
 import androidx.recyclerview.widget.RecyclerView
-import com.andhiratobing.movieapp.data.common.Constants
+import com.andhiratobing.domain.models.MovieDomain
 import com.andhiratobing.movieapp.databinding.ItemMovieBinding
-import com.andhiratobing.movieapp.domain.models.MovieDomain
+import com.andhiratobing.remote.common.Constants.BASE_IMAGE_URL
 import com.bumptech.glide.Glide
 
 class MovieViewHolder(private val binding: ItemMovieBinding) :
@@ -14,7 +14,7 @@ class MovieViewHolder(private val binding: ItemMovieBinding) :
             binding.tvTitleMovie.text = movie.originalTitle
 
             Glide.with(itemView)
-                .load("${Constants.BASE_IMAGE_URL}${movie.posterPath}")
+                .load("${BASE_IMAGE_URL}${movie.posterPath}")
                 .into(ivImagePosterPath)
         }
     }
