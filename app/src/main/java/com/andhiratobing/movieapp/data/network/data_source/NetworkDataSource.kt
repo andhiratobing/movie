323ba21d1aecd8c27f9503f06470aee6aef53a5d
ratobing <ratobing6@gmail.com>
2021-11-06 09:36:52 +0700

    move to module data/remote

--- app/src/main/java/com/andhiratobing/movieapp/data/network/data_source/NetworkDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.andhiratobing.movieapp.data.network.data_source
-
-import com.andhiratobing.movieapp.data.network.dto.MovieDto
-
-interface NetworkDataSource {
-
-    suspend fun fetchPopularMovie(): List<MovieDto>
-}
