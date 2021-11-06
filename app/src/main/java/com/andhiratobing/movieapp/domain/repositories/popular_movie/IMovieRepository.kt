7fee6ff94104e3a756835abcecad16224a8b06c3
ratobing <ratobing6@gmail.com>
2021-11-06 09:40:22 +0700

    move to domain module

--- app/src/main/java/com/andhiratobing/movieapp/domain/repositories/popular_movie/IMovieRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.andhiratobing.movieapp.domain.repositories.popular_movie
-
-import com.andhiratobing.movieapp.domain.models.MovieDomain
-
-interface IMovieRepository {
-
-    suspend fun fetchPopularMovie(): List<MovieDomain>
-}
