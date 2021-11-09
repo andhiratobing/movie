7d8476ee99b0c3841cae42c25e0c936babb2fae0
ratobing <ratobing6@gmail.com>
2021-11-09 16:25:11 +0700

    :hammer: update TvShowsDto.kt

--- data/remote/src/main/java/com/andhiratobing/remote/dto/tv_shows/TvShowsDto.kt
+++ data/remote/src/main/java/com/andhiratobing/remote/dto/tv_shows/TvShowsDto.kt
@@ -1,12 +1,14 @@
 package com.andhiratobing.remote.dto.tv_shows
 
+import com.google.gson.annotations.SerializedName
+
 data class TvShowsDto(
-    val backdrop_path: String,
-    val first_air_date: String,
-    val genre_ids: List<Int>,
-    val id: Int,
-    val original_name: String,
-    val overview: String,
-    val poster_path: String,
-    val vote_average: Double
+    @SerializedName("backdrop_path") val backdropPath: String?,
+    @SerializedName("first_air_date") val firstAirDate: String?,
+    @SerializedName("genre_ids") val genreIds: List<Int>?,
+    @SerializedName("id") val id: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("overview") val overview: String?,
+    @SerializedName("poster_path") val posterPath: String?,
+    @SerializedName("vote_average") val voteAverage: Double?
 )
