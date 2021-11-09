0ca8209619fc47800f0046bc0efb0ff783c53859
ratobing <ratobing6@gmail.com>
2021-11-09 11:08:28 +0700

    :wrench: add IMovieRepository.kt

---
+++ domain/src/main/java/com/andhiratobing/domain/repositories/movie/IMovieRepository.kt
@@ -0,0 +1,10 @@
+package com.andhiratobing.domain.repositories.movie
+
+import com.andhiratobing.domain.models.MovieDomain
+
+interface IMovieRepository {
+
+    suspend fun fetchPopularMovie(): List<MovieDomain>
+    suspend fun fetchUpComingMovie(): List<MovieDomain>
+    suspend fun fetchNowPlayingMovie(): List<MovieDomain>
+}
