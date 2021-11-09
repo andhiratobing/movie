f11fa9a6efbd2a315b62d48e6ae769353a590c6f
ratobing <ratobing6@gmail.com>
2021-11-09 11:11:00 +0700

    :hammer: update MovieUseCase.kt

--- domain/src/main/java/com/andhiratobing/domain/interactor/MovieUseCase.kt
+++ domain/src/main/java/com/andhiratobing/domain/interactor/MovieUseCase.kt
@@ -1,7 +1,11 @@
 package com.andhiratobing.domain.interactor
 
-import com.andhiratobing.domain.interactor.popular_movie.PopularMovieUseCase
+import com.andhiratobing.domain.interactor.movie.now_playing_movie.NowPlayingMovieUseCase
+import com.andhiratobing.domain.interactor.movie.popular_movie.PopularMovieUseCase
+import com.andhiratobing.domain.interactor.movie.up_coming_movie.UpComingMovieUseCase
 
 data class MovieUseCase(
-    val popularMovieUseCase: PopularMovieUseCase
+    val popularMovieUseCase: PopularMovieUseCase,
+    val upComingMovieUseCase: UpComingMovieUseCase,
+    val nowPlayingMovieUseCase: NowPlayingMovieUseCase
 )
