d2a5b8f72e666453f5426f68fcfa13ab42314734
ratobing <ratobing6@gmail.com>
2021-11-09 11:04:48 +0700

    :fire: remove IPopularMovieRepository.kt

--- domain/src/main/java/com/andhiratobing/domain/repositories/popular_movie/IPopularMovieRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.andhiratobing.domain.repositories.popular_movie
-
-import com.andhiratobing.domain.models.MovieDomain
-
-interface IPopularMovieRepository {
-
-    suspend fun fetchPopularMovie(): List<MovieDomain>
-}
