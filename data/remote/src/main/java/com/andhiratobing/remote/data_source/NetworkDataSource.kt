d92f27825975a5617bd2eae0ff8c0d636537b8f9
ratobing <ratobing6@gmail.com>
2021-11-09 13:39:18 +0700

    :hammer: update NetworkDataSource.kt

--- data/remote/src/main/java/com/andhiratobing/remote/data_source/NetworkDataSource.kt
+++ data/remote/src/main/java/com/andhiratobing/remote/data_source/NetworkDataSource.kt
@@ -1,10 +1,18 @@
 package com.andhiratobing.remote.data_source
 
-import com.andhiratobing.remote.dto.MovieDto
+import com.andhiratobing.remote.dto.movie.MovieDto
+import com.andhiratobing.remote.dto.tv_shows.TvShowsDto
 
 interface NetworkDataSource {
 
+    //movie
     suspend fun fetchPopularMovie(): List<MovieDto>
     suspend fun fetchUpComingMovie(): List<MovieDto>
     suspend fun fetchNowPlayingMovie(): List<MovieDto>
+
+
+    //tv shows
+    suspend fun fetchAiringTodayTvShows(): List<TvShowsDto>
+    suspend fun fetchPopularTvShows(): List<TvShowsDto>
+    suspend fun fetchOnTheAirTvShows(): List<TvShowsDto>
 }
