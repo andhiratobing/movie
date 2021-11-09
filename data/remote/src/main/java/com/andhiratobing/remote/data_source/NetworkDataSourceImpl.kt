c8ed0ef6d311145c96d8b4ed1e6e1a703944204a
ratobing <ratobing6@gmail.com>
2021-11-09 13:40:24 +0700

    :hammer: update NetworkDataSourceImpl.kt

--- data/remote/src/main/java/com/andhiratobing/remote/data_source/NetworkDataSourceImpl.kt
+++ data/remote/src/main/java/com/andhiratobing/remote/data_source/NetworkDataSourceImpl.kt
@@ -1,12 +1,17 @@
 package com.andhiratobing.remote.data_source
 
 import com.andhiratobing.remote.api.ApiService
-import com.andhiratobing.remote.dto.MovieDto
+import com.andhiratobing.remote.dto.movie.MovieDto
+import com.andhiratobing.remote.dto.tv_shows.TvShowsDto
 import javax.inject.Inject
 
 class NetworkDataSourceImpl @Inject constructor(
     private val apiService: ApiService
 ): NetworkDataSource {
+
+    /**
+     * Movie
+     */
 
     override suspend fun fetchPopularMovie(): List<MovieDto> {
         return apiService.fetchPopularMovie().result
@@ -18,6 +23,23 @@ class NetworkDataSourceImpl @Inject constructor(
 
     override suspend fun fetchNowPlayingMovie(): List<MovieDto> {
         return apiService.fetchNowPlayingMovie().result
+    }
+
+
+    /**
+     * TV Shows
+     */
+
+    override suspend fun fetchAiringTodayTvShows(): List<TvShowsDto> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun fetchPopularTvShows(): List<TvShowsDto> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun fetchOnTheAirTvShows(): List<TvShowsDto> {
+        TODO("Not yet implemented")
     }
 
 
